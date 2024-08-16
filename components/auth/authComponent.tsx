8cf41727455116942e384b0b887c2c0e0c256dcb
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-08-16 22:22:37 +0545

    refactor: Rename LoginForm component to AuthForm

--- components/auth/authComponent.tsx
+++ components/auth/authComponent.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import imageUrls from "@/constants/Images";
 import Image from "next/image";
 import { Separator } from "../ui/separator";
 import AuthForm from "./form";
+import { useState } from "react";
 
 const AuthComponent = () => {
+  const [isLoginForm, setIsLoginForm] = useState(true);
+
   return (
     <>
       <div className="flex flex-col justify-start items-center h-[35vw] w-[40vw] border-1 border-slate-200 shadow-lg rounded-lg">
@@ -23,19 +28,31 @@ const AuthComponent = () => {
 
         <div className="w-full flex flex-col items-center mt-10">
           <div className="flex justify-evenly w-full font-bold text-2xl">
-            <span>Login</span>
-            <span>Signup</span>
+            <div className="flex flex-col items-center">
+              <button onClick={() => setIsLoginForm(true)}>Login</button>
+              <Separator
+                className="w-full mt-2"
+                color={isLoginForm ? "bg-black" : "bg-border"}
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <button onClick={() => setIsLoginForm(false)}>Signup</button>
+              <Separator
+                className="w-full mt-2"
+                color={!isLoginForm ? "bg-black" : "bg-border"}
+              />
+            </div>
           </div>
         </div>
-        <Separator className="w-[70%] mt-2 " />
 
         <div className="h-full w-full mt-10 flex justify-center">
           <div className="w-[70%]">
-            <AuthForm isLoginForm={false} />
+            <AuthForm isLoginForm={isLoginForm} />
           </div>
         </div>
       </div>
     </>
   );
 };
+
 export default AuthComponent;
--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -5,12 +5,23 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
 import { cn } from "@/lib/utils";
 
+interface SeparatorProps
+  extends React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root> {
+  color?: string;
+}
+
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+  SeparatorProps
 >(
   (
-    { className, orientation = "horizontal", decorative = true, ...props },
+    {
+      className,
+      orientation = "horizontal",
+      decorative = true,
+      color = "bg-border",
+      ...props
+    },
     ref
   ) => (
     <SeparatorPrimitive.Root
@@ -18,8 +29,10 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[2px] w-full" : "h-full w-[2px]",
+        "shrink-0",
+        orientation === "horizontal"
+          ? `h-[2px] w-full ${color}`
+          : `h-full w-[2px] ${color}`,
         className
       )}
       {...props}
