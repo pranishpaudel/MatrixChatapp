6360a4eb523e1752001c7f3d07883c091210516a
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-08-21 15:19:01 +0545

    refactor: Improve authentication middleware
    This commit improves the authentication middleware by adding a JWT validation function. The function verifies the validity of the token using the `jose` library and checks if it contains the necessary user information. If the token is invalid or expired, the middleware clears the JWT cookie and redirects the user to the authentication page. Additionally, the middleware now redirects authenticated users from the `/auth` path to the profile page. This enhances the security of the application and ensures that only authenticated users can access certain routes.

--- middleware.ts
+++ middleware.ts
@@ -8,28 +8,37 @@ export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value || "";
   const REDIRECTION_FOR_UNAUTHENTICATED = `${request.nextUrl.origin}/auth`;
 
+  const jwtValidationFunction = async function (
+    token: string
+  ): Promise<boolean> {
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+      const { payload } = await jwtVerify(token, secret);
+      const verifiedJWT = payload as unknown as jwtVerifiedUser;
+
+      if (!verifiedJWT.email || !verifiedJWT.id) {
+        return false;
+      }
+      return true;
+    } catch (error) {
+      // Clear the JWT cookie if it is invalid or expired
+      const response = NextResponse.next();
+      response.cookies.set("token", "", { maxAge: -1, path: "/" });
+      return false;
+    }
+  };
+
+  const isJwtValid = await jwtValidationFunction(token);
+  if (pathname === "/auth" && isJwtValid) {
+    return NextResponse.redirect(`${request.nextUrl.origin}/profile`);
+  }
   // Bypass the middleware for the /auth path
   if (pathname === "/auth") {
     return NextResponse.next();
   }
 
-  if (!token) {
+  if (!token || !isJwtValid) {
     return NextResponse.redirect(REDIRECTION_FOR_UNAUTHENTICATED);
-  }
-
-  try {
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const { payload } = await jwtVerify(token, secret);
-    const verifiedJWT = payload as unknown as jwtVerifiedUser;
-
-    if (!verifiedJWT.email) {
-      return NextResponse.redirect(REDIRECTION_FOR_UNAUTHENTICATED);
-    }
-  } catch (error) {
-    // Clear the JWT cookie if it is invalid or expired
-    const response = NextResponse.redirect(REDIRECTION_FOR_UNAUTHENTICATED);
-    response.cookies.set("token", "", { maxAge: -1, path: "/" });
-    return response;
   }
 
   return NextResponse.next();
