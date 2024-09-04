6ffb988b04b17166eb2ef22a83353e12392de61a
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-04 19:25:29 +0545

    refactor: Add lazy loading for images in auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,11 @@
 import AuthComponent from "@/components/auth/authComponent";
+import { ModeToggle } from "@/components/ui/modeToggle";
 
 const page = () => {
   return (
     <div>
       <div className="h-[100vh] w-[100vw]  flex justify-center items-center">
+        <ModeToggle />
         <AuthComponent />
       </div>
     </div>
