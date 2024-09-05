0f443d8ef1eb964d5578370c6d3f8719c221a6df
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-05 09:04:15 +0545

    feat: Add AWS S3 client and configuration
    This commit adds the AWS S3 client and configuration to the project. It includes the addition of the "@aws-sdk/client-s3" dependency in the package.json file. The "awsClientProvider.ts" file is also added, which sets up the S3 client with the necessary credentials and region. This addition enables the application to interact with the AWS S3 service for file storage and retrieval.

---
+++ utils/awsClientProvider.ts
@@ -0,0 +1,9 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const client = new S3Client({
+  region: process.env.AWS_REGION_ID,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET as string,
+  },
+});
