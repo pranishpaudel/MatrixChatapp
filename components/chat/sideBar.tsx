0a12fabb989068f22a69f61e5764f6d42a41f435
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-12 12:10:33 +0545

    Refactor sideBar component to improve layout and add text labels

--- components/chat/sideBar.tsx
+++ components/chat/sideBar.tsx
@@ -2,9 +2,21 @@ import { Separator } from "../ui/separator";
 
 const SideBar = () => {
   return (
-    <div className="h-full flex  justify-end">
-      <Separator className="h-full" orientation="vertical" />
-      <div></div>
+    <div className="flex h-full">
+      {/* Central Area for Text Items */}
+      <div className="flex flex-col justify-start items-start w-full">
+        <div className="text-lg mb-4 text-gray-300 relative left-[10%]">
+          Direct Messages
+        </div>
+        <div className="text-lg text-gray-300 relative left-[10%]">
+          Channels
+        </div>
+      </div>
+
+      {/* Separator */}
+      <div className="flex-shrink-0">
+        <Separator className="h-full" orientation="vertical" />
+      </div>
     </div>
   );
 };
