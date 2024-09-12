86700950d38b364ffbcf37b4b2bc31640aa7d44a
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-12 19:24:30 +0545

    Refactor SideBar component to add a plus icon for direct messages

--- components/chat/sideBar.tsx
+++ components/chat/sideBar.tsx
@@ -1,11 +1,13 @@
+import { Plus } from "lucide-react";
 import ProfileComponent from "./ProfileComponent";
 
 const SideBar = () => {
   return (
     <div className="flex h-full">
       <div className="flex flex-col justify-start items-start w-full">
-        <div className="text-lg mb-4 text-gray-300 relative left-[10%]">
+        <div className="flex items-center text-lg mb-4 text-gray-300 relative left-[10%]">
           Direct Messages
+          <Plus className="mr-2 ml-4 cursor-pointer hover:scale-150 transition-transform duration-200" />
         </div>
         <div className="text-lg text-gray-300 relative left-[10%]">
           Channels
