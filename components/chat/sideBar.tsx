8a818cc67b0945c128a2097bf635c7a942d941f4
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-12 11:57:31 +0545

    Refactor sideBar component to fix alignment issue

--- components/chat/sideBar.tsx
+++ components/chat/sideBar.tsx
@@ -2,8 +2,9 @@ import { Separator } from "../ui/separator";
 
 const SideBar = () => {
   return (
-    <div className="h-full flex justify-end">
+    <div className="h-full flex  justify-end">
       <Separator className="h-full" orientation="vertical" />
+      <div></div>
     </div>
   );
 };
