7d7e3d34455fa6178d4b19a946a5ac154c9a6b47
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-14 15:14:18 +0545

    Refactor package.json to include "type" field for module support

--- chatServer/src/index.ts
+++ chatServer/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hi");
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error("Failed to start server:", err);
+  });
