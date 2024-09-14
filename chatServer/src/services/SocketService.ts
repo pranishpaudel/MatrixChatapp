2fa5a98fc3d6cbe70a0872768e37d93bf9c37028
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-14 21:10:25 +0545

    Refactor Redis connection and message publishing in SocketService

--- chatServer/src/services/SocketService.ts
+++ chatServer/src/services/SocketService.ts
@@ -1,12 +1,11 @@
 import { Server } from "socket.io";
 import Redis from "ioredis";
 
-// Connect to Redis
 const serviceUri =
   "rediss://default:AVNS_bZyrZ7T9-2PsZX49E8H@caching-972a5c3-sindsa26-d146.l.aivencloud.com:10664";
 const pub = new Redis(serviceUri);
 const sub = new Redis(serviceUri);
-// Error handling for Redis connections
+
 pub.on("error", (err) => {
   console.error("Redis pub error:", err);
 });
@@ -15,10 +14,7 @@ sub.on("error", (err) => {
   console.error("Redis sub error:", err);
 });
 
-// Subscribe to the "MESSAGES" channel
 sub.subscribe("MESSAGES");
-
-// Listen for messages on the "MESSAGES" channel
 
 class SocketService {
   private _io: Server;
@@ -31,8 +27,13 @@ class SocketService {
         origin: "*",
       },
     });
+
+    // Single listener for Redis messages
     sub.on("message", (channel, message) => {
-      this._io.emit("event:message", JSON.parse(message));
+      if (channel === "MESSAGES") {
+        console.log(`Received message from ${channel}: ${message}`);
+        this._io.emit("message", JSON.parse(message));
+      }
     });
   }
 
@@ -40,6 +41,7 @@ class SocketService {
     console.log("SocketService initListeners");
     this._io.on("connection", (socket) => {
       console.log("New client connected", socket.id);
+
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("New message received", message);
         try {
@@ -52,12 +54,7 @@ class SocketService {
           console.error("Failed to publish message:", err);
         }
       });
-      sub.on("message", (channel, message) => {
-        if (channel === "MESSAGES") {
-          console.log(`Received message from ${channel}: ${message}`);
-          this._io.emit("message", JSON.parse(message));
-        }
-      });
+
       socket.on("disconnect", () => {
         console.log("Client disconnected");
       });
