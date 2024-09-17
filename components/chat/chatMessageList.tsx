ee36cec72842144fe90c9937f96b6874c2926537
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-17 18:26:18 +0545

    Refactor chatMessageList component to remove console log statement

--- components/chat/chatMessageList.tsx
+++ components/chat/chatMessageList.tsx
@@ -99,10 +99,7 @@ const ChatMessageList: React.FC = () => {
           offlineMessage: chat.offlineMessage,
         })),
     ];
-    ////console log all offline chat too
-    offlineChatHistory.forEach((element) => {
-      console.log(element);
-    });
+
     setChats(newChatHistory);
   }, [
     offlineChatHistory,
