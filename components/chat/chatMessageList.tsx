97e9faea3a7976b281c1841871887780ca80af0e
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-09-17 16:45:11 +0545

    Refactor chatMessageList component to remove unused variable

--- components/chat/chatMessageList.tsx
+++ components/chat/chatMessageList.tsx
@@ -74,7 +74,6 @@ const ChatMessageList: React.FC = () => {
   }, [receiverData, offlineChatHistory, firstPaint]);
 
   useEffect(() => {
-    firstPaint;
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop =
         chatContainerRef.current.scrollHeight;
